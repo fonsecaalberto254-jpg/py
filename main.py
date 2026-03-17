62ffe503842d113649ba6f25e007d29a230f1644
Bryan (cantaloupe) <fonsecaalberto254@gmail.com>
2026-03-17 14:09:30 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 
-import random as r
-import time as t    
-
-
 #Rank	Peak	Title	                    Worldwide gros  Year	
 #1	    1	    Avatar	                    $2,923,710,708	2009  
 #2	    1	    Avengers: Endgame	        $2,797,501,328	2019	
 #3	    3	    Avatar: The Way of Water	$2,334,484,620	2022
-
-
 class stuff:
     b=75
     r=35
@@ -21,11 +15,17 @@ class stuff:
             if j == "yes":
                 k=input("which one do you want to know about? 1, 2 or 3: ")
                 if k == "1":
+                    print("\n")
                     print("Avatar | is the most grossing movie of all time with a worldwide gross o |f $2,923,710,708 in | 2009")
+                    print("make blank line apppear below\n")
                 elif k == "2":
+                    print("\n")
                     print("Avengers: Endgame | is the second most grossing movie of all time with a worldwide gross of | $2,797,501,328 in | 2019")
+                    print("make blank line apppear below\n")
                 elif k == "3":
+                    print("\n")
                     print("Avatar: The Way of Water | is the third most grossing movie of all time with a worldwide gross of | $2,334,484,620  |in 2022")
+                    print("make blank line apppear below\n")
                 print("want to know another one?")
                 z=input("yes or no: ")
                 if z == "yes":
